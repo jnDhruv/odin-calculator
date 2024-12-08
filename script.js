6c4cbf068cf335fd2a79ad941ad45f9eb297200a
Dhruv <dhruvjain.dev@gmail.com>
2024-12-08 14:53:30 +0530

    Add function to numeric buttons

--- script.js
+++ script.js
@@ -24,3 +24,23 @@ function operate(op, num1, num2) {
     }
     return result;
 }
+
+function populateNumber(numStr) {
+    let prevNum = parseInt(display.textContent)
+    let newNum = prevNum*10 + parseInt(numStr);
+    display.textContent = newNum;
+}
+
+let num1 = 0;
+let num2 = 0;
+let op;
+
+let display = document.querySelector('.display');
+
+const numericButtons = document.querySelectorAll(".num");
+
+for (let numBtn of numericButtons) {
+    numBtn.addEventListener("click", (e) => {
+        populateNumber(e.target.id);
+    })
+}
