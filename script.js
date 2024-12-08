1586271cedc398f711f3b0f1e4000fc657b99ec4
Dhruv <dhruvjain.dev@gmail.com>
2024-12-08 16:07:56 +0530

    Create the operational flow of the program
    - Added functions to operator buttons.
    - Variable assignment, evaluation and reset when clicking '='
    - Added function to clear button
    
    Calculator is now operating normally,
    But these should be changed:
    - When an operator is pressed again without entering the secondOperand, the program does nothing.
        It should perform the operation clicked on the firstOperand itself and display the result
        example: clicking '1' '+' '+' should display '2', another '+' should display '4' and so on.
    - Divide by zero error

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function divide(num1, num2) {
 function operate(op, num1, num2) {
     let result;
     switch (op) {
-        case '+': result = add(num1, num2);
-        case '-': result = subtract(num1, num2);
-        case 'x': result = multiply(num1, num2);
-        case '/': result = divide(num1, num2);
+        case '+': result = add(num1, num2); break;
+        case '-': result = subtract(num1, num2); break;
+        case '*': result = multiply(num1, num2); break;
+        case '/': result = divide(num1, num2); break;
     }
     return result;
 }
@@ -29,18 +29,63 @@ function populateNumber(numStr) {
     let prevNum = parseInt(display.textContent)
     let newNum = prevNum*10 + parseInt(numStr);
     display.textContent = newNum;
+
+    if (!op) {
+        firstOperand = newNum;
+    } else {
+        secondOperand = newNum;
+    }
 }
 
-let num1 = 0;
-let num2 = 0;
-let op;
+function updateOp(operator) {
+    op = operator;
+    display.textContent = "0";
+}
+
+let firstOperand = 0;
+let secondOperand = 0;
+let op = '';
 
 let display = document.querySelector('.display');
 
-const numericButtons = document.querySelectorAll(".num");
+const allButtons = document.querySelectorAll(".btn");
 
-for (let numBtn of numericButtons) {
-    numBtn.addEventListener("click", (e) => {
-        populateNumber(e.target.id);
-    })
+for (let btn of allButtons) {
+
+    const btnClasses = btn.classList;
+
+    if (btnClasses.contains("num")) {
+        btn.addEventListener("click", (e) => {
+            populateNumber(e.target.id);
+        });
+    }
+
+    else if (btnClasses.contains("op")) {
+        btn.addEventListener("click", (e) => {
+            updateOp(e.target.id);
+        });
+    }
+
+    else if (btnClasses.contains("equals")) {
+        btn.addEventListener("click", () => {
+            if (!op) {
+                return;
+            }
+
+            let result = operate(op, firstOperand, secondOperand);
+            display.textContent = result;
+            firstOperand = result;
+            secondOperand = 0;
+            op = '';
+        });
+    }
+
+    else if (btnClasses.contains("clear")) {
+        btn.addEventListener("click", () => {
+            firstOperand = 0;
+            secondOperand = 0;
+            op = '';
+            display.textContent = '0';
+        });
+    }
 }
